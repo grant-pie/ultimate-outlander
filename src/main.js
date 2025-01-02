0eac5bb3a225927d4533d0b931e54f460ca226ff
grant-pie <grant.pieterse1@gmail.com>
2025-01-02 17:50:33 +0200

    Added Bootstrap 5

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
-import './style.css'
+//import './style.css'
 import App from './App.vue'
+
+// Import Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Import Bootstrap JS
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 createApp(App).mount('#app')
