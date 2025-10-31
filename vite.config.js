e709edfae0f7ea9f1bbb293572c06ad9f835e4c5
grant-pie <grant.pieterse1@gmail.com>
2025-10-31 09:58:50 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/ultimate-outlander/', // Replace with your actual repository name
+  base: '/', // Replace with your actual repository name
 })
